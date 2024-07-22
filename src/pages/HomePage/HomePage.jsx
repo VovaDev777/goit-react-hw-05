ba2a1717f142ee05abdf1b8c6b56e0b52aeeea8b
VovaDev777 <polvovater@gmail.com>
2024-07-22 13:56:39 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { fetchData } from '../../helpers/fetchData';
+import { useEffect, useState } from 'react'
 
 const HomePage = () => {
 
-    
+    useEffect(() => {
+        async function fetchFilms() {
+            try {
+                const films = await fetchData();
+                console.log(films.data.results)
+            } catch (error) {
+                console.log(error)
+            }
+        } 
+        fetchFilms();
+    }, []);
+
   return (
     <div>
+        <div>
+            <p></p>
+            <p></p>
+        </div>
 
     </div>
   )
